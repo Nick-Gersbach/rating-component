614bc09688c30b927e1ebea285014dc5c58247cf
Nicholas Gersbach <54191233+Nick-Gersbach@users.noreply.github.com>
2022-09-26 17:21:01 -0700

    html and js changes

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const numberBtn = document.querySelector(".btn");
+const form = document.querySelector("form");
 
+numberBtn.addEventListener("click", btnBg);
+form.addEventListener("submit", submitFeedback);
+
+function btnBg(e) {
+  numberBtn.style.background = "#fc7614";
+  numberBtn.style.color = "#fff";
+  console.log("Button Clicked");
+  e.preventDefault();
+}
+
+function submitFeedback(e) {
+  console.log("form submitted");
+  e.preventDefault();
+}
