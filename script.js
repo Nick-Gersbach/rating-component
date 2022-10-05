cd0872a53596bd5786dbf8712a36e401b8d388aa
Nicholas Gersbach <54191233+Nick-Gersbach@users.noreply.github.com>
2022-10-04 19:31:41 -0700

    desktop-completed

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-const numberBtn = document.querySelector(".btn");
-const form = document.querySelector("form");
+const ratingForm = document.querySelector(".rating-container");
+const thankYouCard = document.querySelector(".thankyou-component");
 
-numberBtn.addEventListener("click", btnBg);
-form.addEventListener("submit", submitFeedback);
+const submitBtn = document
+  .getElementById("submit-btn")
+  .addEventListener("click", function (e) {
+    //Prevent Form From Submitting
+    e.preventDefault();
 
-function btnBg(e) {
-  numberBtn.style.background = "#fc7614";
-  numberBtn.style.color = "#fff";
-  console.log("Button Clicked");
-  e.preventDefault();
-}
+    //Input Variables
+    const rating1 = document.getElementById("rating-1");
+    const rating2 = document.getElementById("rating-2");
+    const rating3 = document.getElementById("rating-3");
+    const rating4 = document.getElementById("rating-4");
+    const rating5 = document.getElementById("rating-5");
+    const dynamicNumber = document.getElementById("dynamic-number");
 
-function submitFeedback(e) {
-  console.log("form submitted");
-  e.preventDefault();
-}
+    if (rating1.checked === true) {
+      dynamicNumber.innerHTML = rating1.value;
+    } else if (rating2.checked === true) {
+      dynamicNumber.innerHTML = rating2.value;
+    } else if (rating3.checked === true) {
+      dynamicNumber.innerHTML = rating3.value;
+    } else if (rating4.checked === true) {
+      dynamicNumber.innerHTML = rating4.value;
+    } else if (rating5.checked === true) {
+      dynamicNumber.innerHTML = rating5.value;
+    }
+
+    ratingForm.style.display = "none";
+    thankYouCard.style.display = "flex";
+  });
